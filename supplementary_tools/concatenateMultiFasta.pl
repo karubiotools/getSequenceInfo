13343c61b226855a7a793884e15b950061683487
karubiotools <69260399+karubiotools@users.noreply.github.com>
2021-09-07 13:07:16 -0400

    Create concatenateMultiFasta.pl

---
+++ supplementary_tools/concatenateMultiFasta.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+use strict;
+
+################################################################
+# Script allowing to concatenate multiFasta file,
+# generating an output file containing a single sequence
+################################################################
+
+# example of use: perl concatenateMultiFasta.pl multiFasta_file.fasta
+# other example:  perl concatenateMultiFasta.pl *.fasta
+
+my @listFastaFiles = @ARGV;
+
+foreach my $multiFasta ( @listFastaFiles ) {
+        my $outFasta = 'concatenate_'.$multiFasta ;
+        open(FILE,"<$multiFasta") || die ("Error opening $multiFasta $!");
+        open(OUT, '>', $outFasta) or die $!;
+        print OUT ">concatenation $multiFasta\n";
+        while (my $row = <FILE>) {
+                chomp $row;
+                if ($row=~m/^>/){
+                }
+                else{
+                    print OUT "$row\n";
+                }
+        }
+}
+
+close(FILE);
+close(OUT);
